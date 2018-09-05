09d9bdb6fbf3ca9984ea0d14da45559322f25501
Euan <thecableton@gmail.com>
2018-09-05 20:15:35 +0100

    Initial Commit

---
+++ models/traveller.js
@@ -0,0 +1,51 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) => {
+    return journey.startLocation;
+  })
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+  });
+};
+
+Traveller.prototype.getModesOfTransport = function () {
+  return this.journeys.map((journey) =>{
+    return journey.transport;
+  });
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  });
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) =>{
+    return journey.distance >= minDistance;
+  })
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance;
+  }, 0);
+
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  return this.journeys.reduce((modesOfTransport, journey) => {
+    if(!modesOfTransport.includes(journey.transport))
+      modesOfTransport.push(journey.transport);
+    return modesOfTransport;
+  }, []);
+};
+
+
+module.exports = Traveller;
